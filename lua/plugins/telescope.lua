55297eb7d331cd5c2d4eda2c0ca229dbf6e58460
dajmic <dan.michell1000@gmail.com>
2025-10-20 21:14:04 +0100

    feat: add telescope resume command

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,8 +15,10 @@ local config = function()
 	local keymap = vim.keymap
 
 	keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope files" })
+	keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope files" })
 	keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Telescope grep" })
-	keymap.set("n", "<leader>b", "<cmd>Telescope buffers<cr>", { desc = "Telescope buffers" })
+	keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>", { desc = "Telescope grep" })
+	keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Telescope buffers" })
 	keymap.set("n", "<leader>fs", "<cmd>Telescope git_status<cr>", { desc = "Telescope git status" })
 	keymap.set("n", "<leader>fc", "<cmd>Telescope git commits<cr>", { desc = "Telescope git commits" })
 end
